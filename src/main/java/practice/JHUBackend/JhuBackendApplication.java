7ca900fdd8974fa258d060b495a8173821ed6580
Eugene Kim <ekim1707@gmail.com>
2020-08-24 16:44:17 -0400

    test

--- src/main/java/practice/JHUBackend/JhuBackendApplication.java
+++ src/main/java/practice/JHUBackend/JhuBackendApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JhuBackendApplication {
+public abstract class JhuBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JhuBackendApplication.class, args);
+		returnString();
+	}
+
+	public static void returnString() {
+		System.out.println("Hello");
 	}
 
 }
